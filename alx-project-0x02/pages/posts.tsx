217903517da27b71d8a3a26651e7b1e2337ccf09
Hillary Robert <roberthillary30@gmail.com>
2025-07-27 18:58:04 +0300

    Implement a PostCard Component

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,39 +1,39 @@
-import Header from '@/components/layout/Header'
-import React, { useEffect, useState } from 'react';
+import Header from '@/components/layout/Header';
+import React from 'react';
 import PostCard from '@/components/common/PostCard';
 import { PostProps } from '@/interfaces';
 
+interface PostsPageProps {
+  posts: PostProps[];
+}
 
-
-const Posts: React.FC = () => {
-  const [posts, setPosts] = useState<PostProps[]>([]);
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=10');
-      const data = await res.json();
-
-      const formattedPosts = data.map((post: any) => ({
-        title: post.title,
-        content: post.body,
-        userId: post.userId,
-      }));
-
-      setPosts(formattedPosts);
-    };
-
-    fetchPosts();
-  }, []);
-
+const Posts: React.FC<PostsPageProps> = ({ posts }) => {
   return (
     <div className="p-6">
-      <Header/>
+      <Header />
       <h1 className="text-3xl font-bold mb-6">Posts</h1>
       {posts.map((post, index) => (
         <PostCard key={index} {...post} />
       ))}
     </div>
   );
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=10');
+  const data = await res.json();
+
+  const posts = data.map((post: any) => ({
+    title: post.title,
+    content: post.body,
+    userId: post.userId,
+  }));
+
+  return {
+    props: {
+      posts,
+    },
+  };
 };
 
 export default Posts;
